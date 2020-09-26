d043bcaf094d706ad40b028e89236a8aa6325cdf
Dmitrii Piataikin <dpiataikin@gmail.com>
2020-09-26 15:55:39 +0300

    do some tests in main method

--- 1-lab/src/main/java/Main.java
+++ 1-lab/src/main/java/Main.java
@@ -3,6 +3,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        ArrayList<String> arrString = new ArrayList<>();
+        arrString.add("1");
+        arrString.add("2");
+        arrString.add("3");
+        System.out.println(arrString.toString());
+        System.out.println(arrString.get(0));
+        System.out.println(arrString.remove("4"));
+        System.out.println(arrString.remove("3"));
+        System.out.println(arrString.toString());
+
+        ArrayList<Integer> arrInteger = new ArrayList<>();
+        arrInteger.add(1);
+        arrInteger.add(2);
+        arrInteger.add(3);
+        System.out.println(arrInteger.toString());
+        arrInteger.remove((Integer)1);
+        System.out.println(arrInteger.toString());
     }
 }
