4bdb9253df3016e2e2786e2292230cbff7811393
dpiat <dpiataikin@gmail.com>
2020-10-21 15:34:57 +0300

    add setup

--- 3-lab/src/main/java/Tester.java
+++ 3-lab/src/main/java/Tester.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tester {
+
+    private Tester() {};
+
+    private List<Object> arrayList;
+    private List<Object> linkedList;
+
+    private void setup(int N) {
+        arrayList = new ArrayList<Object>();
+        for (int i = 0; i < N; i++) {
+            arrayList.add(new Object());
+        }
+        linkedList.addAll(arrayList);
+    }
+
+    public void testPerformance() {
+
+    }
+
 }
